7a0c5f4d10c2c37be9ca128767a2d565709bb290
David Cantoran <davidcantoran36@gmail.com>
2019-04-18 21:38:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,8 @@ class Scraper
   def get_page 
     
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-    flatiron = Nokogiri::
+    flatiron = Nokogiri::HTML(html)
+    flatiron
     
   end 
   
